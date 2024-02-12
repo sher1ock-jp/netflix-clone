2fd42ee706235444b4fead4551f00b0c121d66b4
sher1ock-jp <takaaaaadayo@gmail.com>
2024-02-12 14:50:40 +0900

    Initial Commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by Takayuki Ono on 2024/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
