204fb6f1f7230dcfeb6e641c77d4833c17527e56
sher1ock-jp <takaaaaadayo@gmail.com>
2024-02-15 03:22:59 +0900

    add initial scroll

--- netflix-clone/Controllers/HomeViewController.swift
+++ netflix-clone/Controllers/HomeViewController.swift
@@ -44,4 +44,12 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
 }
