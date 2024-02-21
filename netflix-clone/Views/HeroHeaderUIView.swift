6b37e54a67f47fab93256d46803004a902377750
sher1ock-jp <takaaaaadayo@gmail.com>
2024-02-21 14:04:16 +0900

    add play button

--- netflix-clone/Views/HeroHeaderUIView.swift
+++ netflix-clone/Views/HeroHeaderUIView.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class HeroHeaderUIView: UIView {
     
+    private let playButton: UIButton = {
+        
+        let button = UIButton()
+        button.setTitle("Play", for: .normal)
+        button.layer.borderColor = UIColor.systemBackground.cgColor
+        button.layer.borderWidth = 1
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+        
+    }()
+    
     private let heroImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -17,10 +28,36 @@ class HeroHeaderUIView: UIView {
         return imageView
     }()
     
+    private func addGradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [
+            UIColor.clear.cgColor,
+            UIColor.systemBackground.cgColor,
+        ]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(heroImageView)
+        addGradient()
+        addSubview(playButton)
+        applyConstraints()
     }
+    
+    private func applyConstraints() {
+        
+        let playButtonConstraints = [
+            playButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 25),
+            playButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20)
+        ]
+        
+        NSLayoutConstraint.activate(playButtonConstraints)
+        
+    }
+    
+    
     
     override func layoutSubviews() {
         super.layoutSubviews()
